faf7aa79c397caa7f13d80774e90bce1d7f93aa3
tyeung <tyeung@walmartlabs.com>
2016-06-10 17:21:20 -0700

    added various view controllers and the compose buttons - step 5

--- Tumblr/TabBarViewController.swift
+++ Tumblr/TabBarViewController.swift
@@ -13,16 +13,45 @@ class TabBarViewController: UIViewController {
     @IBOutlet weak var contentView: UIView! //the view where everything is shown
 
     var homeViewController: UIViewController! //view controller for the 'home' button
+    var searchViewController: UIViewController! //view controller for the 'search' button
+    var composeViewController: UIViewController! //view controller for the 'compose' button
+    var acctViewController: UIViewController! //view controller for the 'account' button
+    var trendingViewController: UIViewController! //view controller for the 'trending' button
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
+        //home view
         homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
-        addChildViewController(homeViewController)
-        contentView.addSubview(homeViewController.view)
+            addChildViewController(homeViewController)
+            contentView.addSubview(homeViewController.view)
         homeViewController.didMoveToParentViewController(self)
+        
+        //search view
+        searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
+            addChildViewController(searchViewController)
+            contentView.addSubview(searchViewController.view)
+        searchViewController.didMoveToParentViewController(self)
+        
+        //compose view
+        composeViewController = storyboard.instantiateViewControllerWithIdentifier("ComposeViewController")
+            addChildViewController(composeViewController)
+            contentView.addSubview(composeViewController.view)
+        composeViewController.didMoveToParentViewController(self)
+        
+        //acct view
+        acctViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
+            addChildViewController(acctViewController)
+            contentView.addSubview(acctViewController.view)
+        acctViewController.didMoveToParentViewController(self)
+        
+        //trending view
+        trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+            addChildViewController(trendingViewController)
+            contentView.addSubview(trendingViewController.view)
+        trendingViewController.didMoveToParentViewController(self)
     }
 
     override func didReceiveMemoryWarning() {
