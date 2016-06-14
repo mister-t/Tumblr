36cd2ec704ec1a2ad41e00f1f15f09c2e16e2db3
tyeung <tyeung@walmartlabs.com>
2016-06-13 22:19:02 -0700

    Animated the login view with email text field and keyboard

--- Tumblr/LoginViewController.swift
+++ Tumblr/LoginViewController.swift
@@ -9,9 +9,38 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var fieldParentView: UIView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    var
+        initialY: CGFloat!,
+        offSet  : CGFloat!
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        fieldParentView.frame.origin.y = initialY + offSet
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        fieldParentView.frame.origin.y = initialY
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //make the blue blinking cursor disappear
+        emailTextField.tintColor = UIColor.clearColor()
+        emailTextField.becomeFirstResponder()
+        
+        //save parent view's y position and
+        //default offSet to -50 so that it goes up
+        initialY = fieldParentView.frame.origin.y
+        offSet = -50
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name:UIKeyboardWillHideNotification, object: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -25,6 +54,9 @@ class LoginViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    @IBAction func onTapOutside(sender: AnyObject) {
+        view.endEditing(true)
+    }
     /*
     // MARK: - Navigation
 
